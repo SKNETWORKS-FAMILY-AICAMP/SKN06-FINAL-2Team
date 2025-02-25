ef3bb2a371c7296b0466396b841e8cc0cd3c60d6
Realoil <chungyuj33@gmail.com>
2025-02-25 17:13:50 +0900

    add: 카카오페이지 웹소설 전체 크롤링 코드 및 일부 데이터 추가
    - url 크롤링 코드 추가
    - 상세 크롤링 코드 추가
    - 크롤링 전반에 쓰이는 모듈 추가
    - BL, 무협, 드라마, 현판 데이터 추가
    - 로맨스 크롤링용 전체 url 데이터, 랭킹 url 데이터 추가

---
+++ data/webnovel_kkopage/webnovel_kkopage_detail.py
@@ -0,0 +1,91 @@
+# webnovel_kkopage 폴더에서 돌리기
+from module.crawling_detail import crawling_detail
+from module.login import kakao_login
+import logging
+import pickle
+import json
+
+driver = kakao_login(visible=False)
+
+genre_names = [
+    # "fantasy",
+    # "modern_fantasy",
+    "romance",
+    # "romance_fantasy",
+    # "martial_arts",
+    # "bl",
+    # "drama",
+]
+
+
+for genre in genre_names:
+    with open(f"total_links/{genre}_url.pkl", "rb") as f:
+        total_link = pickle.load(f)
+    with open(f"ranking_ids/{genre}_url.pkl", "rb") as f:
+        ranking_link = pickle.load(f)
+    logging.info(f"{genre} 디테일 크롤링 시작")
+
+    webnovels = []
+    for link in total_link:
+        try:
+            webnovel = crawling_detail(link, driver)
+            logging.info(f"{webnovel["title"]} 크롤링 완료")
+        except Exception as e:
+            logging.error(f"{link} 크롤링을 완료하지 못했습니다.\n{e}", exc_info=True)
+        if webnovel["url"] in ranking_link:
+            webnovel["ranking"] = ranking_link.index(webnovel["url"]) + 1
+        webnovels.append(webnovel)
+
+    driver.quit()
+
+    error_log = []
+    for novel in webnovels:
+        for key, value in novel.items():
+            if key == "title" and value == "-":
+                logging.info(f"{novel["title"]}, title")
+                error_log.append(novel["url"])
+            elif key == "synopsis" and value == "-":
+                logging.info(f"{novel["title"]}, synopsis")
+                error_log.append(novel["url"])
+            elif key == "status" and value == "-":
+                logging.info(f"{novel["title"]}, status")
+                error_log.append(novel["url"])
+            elif key == "age_rating" and value not in [
+                "전체 이용가",
+                "12세 이용가",
+                "15세 이용가",
+                "19세 이용가",
+            ]:
+                logging.info(f"{novel["title"]}, age_rating, {value}")
+                error_log.append(novel["url"])
+            elif key == "price" and value == "-":
+                logging.info(f"{novel["title"]}, price")
+                error_log.append(novel["url"])
+            elif key == "thumbnail" and value == "-":
+                logging.info(f"{novel["title"]}, thumbnail")
+                error_log.append(novel["url"])
+            elif key == "ganre" and value == "-":
+                logging.info(f"{novel["title"]}, ganre")
+                error_log.append(novel["url"])
+            elif key == "view" and value == "-":
+                logging.info(f"{novel["title"]}, view")
+                error_log.append(novel["url"])
+            elif key == "author" and value == "-":
+                logging.info(f"{novel["title"]}, author")
+                error_log.append(novel["url"])
+            elif key == "episode" and value == "-":
+                logging.info(f"{novel["title"]}, episode")
+                error_log.append(novel["url"])
+            else:
+                pass
+    error_log = list(set(error_log))
+
+    if len(error_log) == 0:
+        logging.info("오류 없음")
+    else:
+        with open(f"error\error_log_{genre}.json", "w", encoding="utf-8") as f:
+            json.dump(error_log, f, indent=4, ensure_ascii=False)
+
+    with open(f"detail\{genre}.json", "w", encoding="utf-8") as f:
+        json.dump(webnovels, f, indent=4, ensure_ascii=False)
+    logging.info("저장 완료")
