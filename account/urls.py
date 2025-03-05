c50ea41ceac0555bfde2cc4fd87eec3075d32ec1
naneunyya <1ee3flowers@naver.com>
2025-03-05 17:13:56 +0900

    chore: 기본 첫 화면

--- account/urls.py
+++ account/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 from django.views.generic import TemplateView
 
+app_name = "account"
 
 urlpatterns = [
     path('signup/', views.user_signup, name='signup'),
